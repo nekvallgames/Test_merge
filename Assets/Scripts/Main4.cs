2e19922997180215c6a48f25abb1549f4ae694cc
Konstantine <x123x@email.ru>
2020-10-02 21:42:21 +0300

    test 2 +

--- Assets/Scripts/Main4.cs
+++ Assets/Scripts/Main4.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Main2 : MonoBehaviour
+public class Main4 : MonoBehaviour
 {
-    // Конструктор
+    // Start is called before the first frame update
     void Start()
     {
         
