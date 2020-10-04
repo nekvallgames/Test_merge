faa0fd60974dab3b569e038f6519c87a9a382ae4
Konstantine <x123x@email.ru>
2020-10-04 13:08:27 +0300

    +

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -7,7 +7,7 @@ public class Main : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        var aaa = 2;
     }
 
     // Update is called once per frame
